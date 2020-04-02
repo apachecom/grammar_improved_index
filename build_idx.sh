ddc7bf779ffab89a95b4498284a9eef16a470657
Alejandro <apachecom91@gmail.com>
2020-04-02 16:21:32 -0300

    build idx bsh

--- build_idx.sh
+++ build_idx.sh
@@ -1,10 +1,10 @@
 cd cmake-build-release-mem-monitor
 make build_indices
 
-./build_indices /d1/apacheco/collections/Escherichia_Coli_p  /d1/apacheco/indices/Escherichia_Coli_p  /d1/apacheco/results/Escherichia_Coli_p
-./build_indices /d1/apacheco/collections/cere_p              /d1/apacheco/indices/cere_p              /d1/apacheco/results/cere_p
-./build_indices /d1/apacheco/collections/coreutils_p              /d1/apacheco/indices/coreutils_p              /d1/apacheco/results/coreutils_p
-./build_indices /d1/apacheco/collections/influenza_p              /d1/apacheco/indices/influenza_p              /d1/apacheco/results/influenza_p
-./build_indices /d1/apacheco/collections/para_p              /d1/apacheco/indices/para_p              /d1/apacheco/results/para_p
-./build_indices /d1/apacheco/collections/kernel_p              /d1/apacheco/indices/kernel_p              /d1/apacheco/results/kernel_p
-./build_indices /d1/apacheco/collections/einstein.en.txt_p              /d1/apacheco/indices/einstein.en.txt_p              /d1/apacheco/results/einstein.en.txt_p
+./build_indices /d1/apacheco/collections/Escherichia_Coli  /d1/apacheco/indices/Escherichia_Coli  /d1/apacheco/results/Escherichia_Coli-mem --benchmark_out=/d1/apacheco/results/build-Escherichia_Coli-mem.csv       --benchmark_out_format=csv
+./build_indices /d1/apacheco/collections/cere              /d1/apacheco/indices/cere              /d1/apacheco/results/cere-mem --benchmark_out=/d1/apacheco/results/build-cere-mem.csv       --benchmark_out_format=csv
+./build_indices /d1/apacheco/collections/coreutils              /d1/apacheco/indices/coreutils              /d1/apacheco/results/coreutils-mem --benchmark_out=/d1/apacheco/results/build-coreutils-mem.csv       --benchmark_out_format=csv
+./build_indices /d1/apacheco/collections/influenza              /d1/apacheco/indices/influenza              /d1/apacheco/results/influenza-mem --benchmark_out=/d1/apacheco/results/build-influenza-mem.csv       --benchmark_out_format=csv
+./build_indices /d1/apacheco/collections/para              /d1/apacheco/indices/para              /d1/apacheco/results/para-mem --benchmark_out=/d1/apacheco/results/build-para-mem.csv       --benchmark_out_format=csv
+./build_indices /d1/apacheco/collections/kernel              /d1/apacheco/indices/kernel              /d1/apacheco/results/kernel-mem  --benchmark_out=/d1/apacheco/results/build-kernel-mem.csv       --benchmark_out_format=csv
+./build_indices /d1/apacheco/collections/einstein.en.txt              /d1/apacheco/indices/einstein.en.txt              /d1/apacheco/results/einstein.en.txt-mem  --benchmark_out=/d1/apacheco/results/build-einstein.en.txt-mem.csv       --benchmark_out_format=csv
