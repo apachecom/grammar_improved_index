e1602a525786584b774a66f6b3c913a1dfa53089
Alejandro <apachecom91@gmail.com>
2020-04-07 01:42:15 -0400

    locate bs improve

--- build_idx.sh
+++ build_idx.sh
@@ -7,7 +7,7 @@ make process_coll
  ./process_coll /d1/apacheco/collections/cere                /d1/apacheco/collections/cere_N0
  ./process_coll /d1/apacheco/collections/coreutils           /d1/apacheco/collections/coreutils_N0
  ./process_coll /d1/apacheco/collections/einstein.en.txt     /d1/apacheco/collections/einstein.en.txt_N0
- ./process_coll /d1/apacheco/collections/influenza           /d1/apacheco/collections/influenza
+ ./process_coll /d1/apacheco/collections/influenza           /d1/apacheco/collections/influenza_N0
  ./process_coll /d1/apacheco/collections/kernel              /d1/apacheco/collections/kernel_N0
  ./process_coll /d1/apacheco/collections/para                /d1/apacheco/collections/para_N0
 #
