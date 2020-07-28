b44119551cdf76cab6e176e9bc9eaf7f8ae3797b
Alejandro <apachecom91@gmail.com>
2020-07-28 16:51:54 -0400

    bug

--- config.sh
+++ config.sh
@@ -2,10 +2,18 @@
 
 for pi in {2,4,8,16}
   do
-    mkdir cmake-build-release-${pi}-${pi}-${pi}
-    cd cmake-build-release-${pi}-${pi}-${pi}
-    cmake -DUSE_PRINT_MODE=ON  -DCMAKE_BUILD_TYPE=Release -DINV_PI_T=${pi} -DINV_PI_T_TRIE=${pi} -DINV_PI_T_QGRAM=${pi}  ../
+#    mkdir cmake-build-release-${pi}-${pi}-${pi}
+#    cd cmake-build-release-${pi}-${pi}-${pi}
+#    cmake -DCMAKE_BUILD_TYPE=Release -DINV_PI_T=${pi} -DINV_PI_T_TRIE=${pi} -DINV_PI_T_QGRAM=${pi}  ../
+#    make
+#    cd ..
+
+    mkdir cmake-build-release-${pi}-${pi}-${pi}-mem
+    cd cmake-build-release-${pi}-${pi}-${pi}-mem
+    cmake -USE_MEM_MONITOR=ON -DUSE_PRINT_MODE=ON  -DCMAKE_BUILD_TYPE=Release -DINV_PI_T=${pi} -DINV_PI_T_TRIE=${pi} -DINV_PI_T_QGRAM=${pi}  ../
     make
+    cd ..
+
 done
 
 
