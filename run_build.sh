d329f05409b0f47e2e3dc92de2490db32205b189
Alejandro <apachecom91@gmail.com>
2020-07-29 01:59:52 -0400

    build bash

--- run_build.sh
+++ run_build.sh
@@ -28,17 +28,17 @@ for pi in {2,4,8,16,32}
   do
     cd cmake-build-release-${pi}-${pi}-${pi}-mem
     make build_indices
-     mkdir /d1/apacheco/results/mem/pi${pi}
-     mkdir /d1/apacheco/results/build/pi${pi}
-    ./build_indices /d1/apacheco/collections/einstein.en.txt_N0_10MB /d1/apacheco/built-indices/pi${pi}/einstein.en.txt_N0_10MB /d1/apacheco/results/mem/pi${pi}/mem-einstein.en.txt_N0_10MB --benchmark_out=/d1/apacheco/results/build/pi${pi}/build-einstein.en.txt_N0_10MB.csv --benchmark_out_format=csv
+#     mkdir /d1/apacheco/results/mem/pi${pi}
+#     mkdir /d1/apacheco/results/build/pi${pi}
+#    ./build_indices /d1/apacheco/collections/einstein.en.txt_N0_10MB /d1/apacheco/built-indices/pi${pi}/einstein.en.txt_N0_10MB /d1/apacheco/results/mem/pi${pi}/mem-einstein.en.txt_N0_10MB --benchmark_out=/d1/apacheco/results/build/pi${pi}/build-einstein.en.txt_N0_10MB.csv --benchmark_out_format=csv
 
 
-#    ./build_indices /d1/apacheco/collections/para_N0            /d1/apacheco/built-indices/pi${pi}/para_N0 /d1/apacheco/results/mem/pi${pi}/mem-para_N0 --benchmark_out=/d1/apacheco/results/build/pi${pi}/build-para_N0.csv --benchmark_out_format=csv
-#    ./build_indices /d1/apacheco/collections/cere_N0            /d1/apacheco/built-indices/pi${pi}/cere_N0 /d1/apacheco/results/mem/pi${pi}/mem-cere_N0 --benchmark_out=/d1/apacheco/results/build/pi${pi}/build-cere_N0.csv --benchmark_out_format=csv
-#    ./build_indices /d1/apacheco/collections/influenza_N0       /d1/apacheco/built-indices/pi${pi}/influenza_N0 /d1/apacheco/results/mem/pi${pi}/mem-influenza_N0 --benchmark_out=/d1/apacheco/results/build/pi${pi}/build-influenza_N0.csv --benchmark_out_format=csv
-#    ./build_indices /d1/apacheco/collections/einstein.en.txt_N0 /d1/apacheco/built-indices/pi${pi}/einstein.en.txt_N0 /d1/apacheco/results/mem/pi${pi}/mem-einstein.en.txt_N0 --benchmark_out=/d1/apacheco/results/build/pi${pi}/build-einstein.en.txt_N0.csv --benchmark_out_format=csv
-#    ./build_indices /d1/apacheco/collections/kernel_N0          /d1/apacheco/built-indices/pi${pi}/kernel_N0 /d1/apacheco/results/mem/pi${pi}/mem-kernel_N0 --benchmark_out=/d1/apacheco/results/build/pi${pi}/build-kernel_N0.csv --benchmark_out_format=csv
-#    ./build_indices /d1/apacheco/collections/coreutils_N0       /d1/apacheco/built-indices/pi${pi}/coreutils_N0 /d1/apacheco/results/mem/pi${pi}/mem-coreutils_N0 --benchmark_out=/d1/apacheco/results/build/pi${pi}/build-coreutils_N0.csv --benchmark_out_format=csv
+    ./build_indices /d1/apacheco/collections/para_N0            /d1/apacheco/built-indices/pi${pi}/para_N0 /d1/apacheco/results/mem/pi${pi}/mem-para_N0 --benchmark_out=/d1/apacheco/results/build/pi${pi}/build-para_N0.csv --benchmark_out_format=csv
+    ./build_indices /d1/apacheco/collections/cere_N0            /d1/apacheco/built-indices/pi${pi}/cere_N0 /d1/apacheco/results/mem/pi${pi}/mem-cere_N0 --benchmark_out=/d1/apacheco/results/build/pi${pi}/build-cere_N0.csv --benchmark_out_format=csv
+    ./build_indices /d1/apacheco/collections/influenza_N0       /d1/apacheco/built-indices/pi${pi}/influenza_N0 /d1/apacheco/results/mem/pi${pi}/mem-influenza_N0 --benchmark_out=/d1/apacheco/results/build/pi${pi}/build-influenza_N0.csv --benchmark_out_format=csv
+    ./build_indices /d1/apacheco/collections/einstein.en.txt_N0 /d1/apacheco/built-indices/pi${pi}/einstein.en.txt_N0 /d1/apacheco/results/mem/pi${pi}/mem-einstein.en.txt_N0 --benchmark_out=/d1/apacheco/results/build/pi${pi}/build-einstein.en.txt_N0.csv --benchmark_out_format=csv
+    ./build_indices /d1/apacheco/collections/kernel_N0          /d1/apacheco/built-indices/pi${pi}/kernel_N0 /d1/apacheco/results/mem/pi${pi}/mem-kernel_N0 --benchmark_out=/d1/apacheco/results/build/pi${pi}/build-kernel_N0.csv --benchmark_out_format=csv
+    ./build_indices /d1/apacheco/collections/coreutils_N0       /d1/apacheco/built-indices/pi${pi}/coreutils_N0 /d1/apacheco/results/mem/pi${pi}/mem-coreutils_N0 --benchmark_out=/d1/apacheco/results/build/pi${pi}/build-coreutils_N0.csv --benchmark_out_format=csv
     cd ..
 done
 
